86dbacf24a590d098831b1625ee4b4826ac21aee
bsimps01 <b.albertsimpson@gmail.com>
2020-11-12 17:03:28 -0800

    added project

---
+++ Vibez/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vibez
+//
+//  Created by Benjamin Simpson on 11/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
