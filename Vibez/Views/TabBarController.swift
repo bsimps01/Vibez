7be1a1ac092cd151e6e347a0a7ebfc4451ab3805
bsimps01 <b.albertsimpson@gmail.com>
2020-12-05 19:15:49 -0800

    protecting api key

---
+++ Vibez/Views/TabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  TabBarController.swift
+//  Vibez
+//
+//  Created by Benjamin Simpson on 12/5/20.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewControllers()
+    }
+    
+    func setupViewControllers() {
+        
+    }
+}
